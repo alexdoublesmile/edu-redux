3c3b79c9d3dbcc2315702b31cd793f914a571119
Александр Плохой <alexplohoy@mail.ru>
2023-04-15 19:07:59 +0300

    add producer

--- src/index.js
+++ src/index.js
@@ -4,12 +4,18 @@ import { BrowserRouter } from "react-router-dom";
 
 import "bootstrap/dist/css/bootstrap.css";
 import App from "./app/App";
+import { Provider } from "react-redux";
+import { createStore } from "./app/store/createStore";
+
+const store = createStore();
 
 reactDom.render(
     <React.StrictMode>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
+        <Provider store={store} >
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </Provider>
     </React.StrictMode>,
     document.querySelector("#root")
 );
