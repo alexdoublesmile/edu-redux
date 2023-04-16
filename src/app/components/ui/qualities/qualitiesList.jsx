3ff67f69a60ca3d8ca2ee4f1247377937981fc25
Александр Плохой <alexplohoy@mail.ru>
2023-04-16 10:19:51 +0300

    refactor quality

--- src/app/components/ui/qualities/qualitiesList.jsx
+++ src/app/components/ui/qualities/qualitiesList.jsx
@@ -1,16 +1,30 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Quality from "./quality";
-import { useSelector } from "react-redux";
-import { getQualitiesLoadingStatus } from "../../../store/qualities";
+import { 
+    // useDispatch, 
+    useSelector 
+} from "react-redux";
+import {
+    getQualitiesByIds,
+    getQualitiesLoadingStatus,
+    // loadQualitiesList
+} from "../../../store/qualities";
 
 const QualitiesList = ({ qualities }) => {
+    // const dispatch = useDispatch();
     const isLoading = useSelector(getQualitiesLoadingStatus());
+    const qualitiesList = useSelector(getQualitiesByIds(qualities));
+    // useEffect(() => {
+    //     dispatch(loadQualitiesList());
+    // }, []);
+
     if (isLoading) return "Loading...";
+
     return (
         <>
-            {qualities.map((qual) => (
-                <Quality key={qual} id={qual} />
+            {qualitiesList.map((qual) => (
+                <Quality key={qual._id} {...qual} />
             ))}
         </>
     );
