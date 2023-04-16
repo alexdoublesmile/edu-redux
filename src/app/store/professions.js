f02cf5e21c056f7f542108dedc4ac44373bea649
Александр Плохой <alexplohoy@mail.ru>
2023-04-16 11:04:01 +0300

    fix professions methods

--- src/app/store/professions.js
+++ src/app/store/professions.js
@@ -1,22 +1,25 @@
 import { createSlice } from "@reduxjs/toolkit";
 import professionService from "../services/profession.service";
+import isOutdated from "../utils/isOutdated";
 
 const professionsSlice = createSlice({
     name: "professions",
     initialState: {
         entities: null,
         isLoading: true,
-        error: null
+        error: null,
+        lastFetch: null
     },
     reducers: {
-        professionsRequested(state) {
+        professionsRequested: (state) => {
             state.isLoading = true;
         },
-        professionsReceived(state, action) {
+        professionsReceived: (state, action) => {
             state.entities = action.payload;
+            state.lastFetch = Date.now();
             state.isLoading = false;
         },
-        professionsRequestFailed(state, action) {
+        professionsRequestFailed: (state, action) => {
             state.error = action.payload;
             state.isLoading = false;
         }
@@ -27,17 +30,26 @@ const { reducer: professionsReducer, actions } = professionsSlice;
 const { professionsRequested, professionsReceived, professionsRequestFailed } =
     actions;
 
-export const loadProfessions = () => async(dispatch) => {
-    dispatch(professionsRequested());
-    try {
-        const { content } = await professionService.get();
-        dispatch(professionsReceived(content));
-    } catch (error) {
-        dispatch(professionsRequestFailed(error.message));
+export const loadProfessionsList = () => async (dispatch, getState) => {
+    const { lastFetch } = getState().professions;
+    if (isOutdated(lastFetch)) {
+        dispatch(professionsRequested());
+        try {
+            const { content } = await professionService.get();
+            dispatch(professionsReceived(content));
+        } catch (error) {
+            dispatch(professionsRequestFailed(error.message));
+        }
     }
 };
 
 export const getProfessions = () => (state) => state.professions.entities;
-export const getProfessionsLoadingStatus = () => (state) => state.professions.isLoading;
+export const getProfessionsLoadingStatus = () => (state) =>
+    state.professions.isLoading;
+export const getProfessionById = (id) => (state) => {
+    if (state.professions.entities) {
+        return state.professions.entities.find((p) => p._id === id);
+    }
+};
 
 export default professionsReducer;
